06d634594eacea2be64beb720430425f5dffeff8
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2024-04-22 16:58:13 +0530

    Update charts.tsx

--- charts.tsx
+++ charts.tsx
@@ -106,6 +106,18 @@ export const BarChart = ({
     return <Bar width={horizontal ? "200%" : ""} options={options} data={data} />;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 interface DoughnutChartProps {
     labels: string[];
     data: number[];
@@ -155,6 +167,17 @@ interface DoughnutChartProps {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
   interface PieChartProps {
     labels: string[];
     data: number[];
@@ -191,3 +214,72 @@ interface DoughnutChartProps {
   
     return <Pie data={pieChartData} options={pieChartOptions} />;
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+interface LineChartProps {
+  data: number[];
+  label: string;
+  backgroundColor: string;
+  borderColor: string;
+  labels?: string[];
+}
+
+export const LineChart = ({
+  data,
+  label,
+  backgroundColor,
+  borderColor,
+  labels = months,
+}: LineChartProps) => {
+  const options: ChartOptions<"line"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  const lineChartData: ChartData<"line", number[], string> = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label,
+        data,
+        backgroundColor,
+        borderColor,
+      },
+    ],
+  };
+
+  return <Line options={options} data={lineChartData} />;
+};
